5b82b7e17066e4e5a21c3af04090446a888da425
AarynSmith <4387096+AarynSmith@users.noreply.github.com>
2022-09-18 12:05:14 -0500

    unify styles

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -125,24 +125,3 @@ const formatTime = (s: number): string => {
   rtn += `${s}s`;
   return rtn;
 };
-const styles = StyleSheet.create({
-  timerButton: {
-    backgroundColor: "#66bb6a",
-    flex: 1,
-    width: "75%",
-    alignItems: "center",
-    justifyContent: "center",
-    minHeight: 35,
-  },
-  deleteButton: {
-    height: 20,
-    elevation: 4,
-    // Material design blue from https://material.google.com/style/color.html#color-color-palette
-    backgroundColor: "#2196F3",
-    borderRadius: 2,
-  },
-  deleteText: {
-    color: "white",
-    fontWeight: "500",
-  },
-});
