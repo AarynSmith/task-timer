e94872f5bfcf2f3afe01f357794fe3ed96caba61
AarynSmith <4387096+AarynSmith@users.noreply.github.com>
2022-09-28 12:06:41 -0500

    add react-devtools for electron

--- public/electron-main.js
+++ public/electron-main.js
@@ -3,6 +3,16 @@ const { app, BrowserWindow } = require("electron");
 const path = require("path");
 const isDev = require("electron-is-dev");
 
+const {
+  default: installExtension,
+  REACT_DEVELOPER_TOOLS,
+} = require("electron-devtools-installer");
+app.whenReady().then(() => {
+  installExtension(REACT_DEVELOPER_TOOLS)
+    .then((name) => console.log(`Added Extension:  ${name}`))
+    .catch((err) => console.log("An error occurred: ", err));
+});
+
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
