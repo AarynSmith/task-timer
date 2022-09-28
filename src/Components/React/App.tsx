a97b24f00815c842b8606e5a3569fe72bd52b294
AarynSmith <4387096+AarynSmith@users.noreply.github.com>
2022-09-27 22:13:58 -0500

    inital app setup

--- src/Components/React/App.tsx
+++ src/Components/React/App.tsx
@@ -1,64 +1,23 @@
 import "../Sass/App.scss";
 // React
-import React, { useEffect } from "react";
+import React from "react";
 // Assets
-import logo from "../../Assets/logo.svg";
-import { useLocalStorage } from "../../useLocalStorage";
+
+// import { useLocalStorage } from "../../useLocalStorage";
 
 export default function App() {
-  // you should always use function components!
-  // class components are practically deprecated
+  // const [countHookExample, setCountHookExample] = useLocalStorage("@count", 0);
 
-  // state hooks replace the the state in a function component
-  const [countHookExample, setCountHookExample] = useLocalStorage("@count", 0);
-
-  // effect hooks replace the lifecycle methods in a function component
-  useEffect(
-    () => {
-      console.log("mouted");
-      return () => {
-        console.log("exited");
-      };
-    },
-    [
-      /**
-       * variables that trigger this effect.
-       * if [] triggers only on mounting.
-       * if undefined trigers on every render.
-       */
-    ]
-  );
-
+  const timers = new Array(11).fill("timer");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>
-          <p>
-            You have clicked the button {countHookExample} times{" "}
-            <span
-              className="button"
-              onClick={() => {
-                setCountHookExample(countHookExample + 1);
-                console.log(countHookExample);
-              }}
-            >
-              INC
-            </span>
-          </p>
-        </div>
-      </header>
+    <div className="appContainer">
+      <div className="timerContainer">
+        {timers.map((v, i) => (
+          <div key={i} className="timer">
+            {v}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
