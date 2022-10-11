8a3d20b6f0e9cd278f3d68243a84d3bcacaf64bd
AarynSmith <4387096+AarynSmith@users.noreply.github.com>
2022-10-11 09:22:11 -0500

    fix test

--- src/Components/React/App.test.tsx
+++ src/Components/React/App.test.tsx
@@ -1,11 +1,9 @@
-// import React from "react";
-// import { render } from "@testing-library/react";
-// import App from "./App";
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
 test("renders app", () => {
-  // const { getByText } = render(<App />);
-  //   const { container } = render(<App />);
-  //   const linkElement = getByText(/timer/i);
-  //   console.log(container);
-  //   expect(linkElement).toBeInTheDocument();
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Add Timer/i);
+  expect(linkElement).toBeInTheDocument();
 });
